d3aec1878402f8768d456ab920af359fe457259e
mada-mada <husein.akm@gmail.com>
2026-05-18 22:07:08 +0700

    feat: define API routes for places, menus, orders, users, and projects

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\PlaceController;
@@ -6,15 +7,23 @@ use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserController;
 
-Route::get('/places', [PlaceController::class, 'index']);
-Route::get('/places/{id}', [PlaceController::class, 'show']);
-Route::post('/places', [PlaceController::class, 'store']);
+Route::get('/places', [PlaceController::class, 'index']);          
+Route::post('/places', [PlaceController::class, 'store']);          
+Route::put('/places/{id}', [PlaceController::class, 'update']);   
+Route::delete('/places/{id}', [PlaceController::class, 'destroy']); 
 
-Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/places/{placeId}/menus', [MenuController::class, 'index']);             
+Route::post('/places/{placeId}/menus', [MenuController::class, 'store']);              
+Route::get('/places/{placeId}/menus/{menuId}', [MenuController::class, 'show']);        
+Route::put('/places/{placeId}/menus/{menuId}', [MenuController::class, 'update']);      
+Route::delete('/places/{placeId}/menus/{menuId}', [MenuController::class, 'destroy']); 
 
-Route::post('/projects', [ProjectController::class, 'store']);
-Route::post('/users', [UserController::class, 'store']);
 
-Route::post('/menus/{placeId}', [MenuController::class, 'store']);
-Route::get('/menus/{placeId}', [MenuController::class, 'index']);
+Route::get('/orders', [OrderController::class, 'index']);   
+Route::post('/orders', [OrderController::class, 'store']);  
 
+Route::get('/users', [UserController::class, 'index']);   
+Route::post('/users', [UserController::class, 'store']);  
+
+
+Route::post('/projects', [ProjectController::class, 'store']); 
